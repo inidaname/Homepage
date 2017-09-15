f764470aed43e131732a27b2a27bec4cdfd55cce
Robert Ridley <robertridley@DCs-MacBook-Air.local>
2017-09-15 14:48:07 +0800

    add ejs templating

--- server.js
+++ server.js
@@ -1,23 +1,18 @@
 var express = require('express');
 var app = express();
-var path = require("path");
+// var path = require("path");
 
+app.set('view engine', 'ejs');
 
 app.get('/', function(req, res){
-	res.sendFile(path.join(__dirname+'/views/index.html'));
+	res.render('index');
 });
 
 app.get('/signup', function(req, res){
-	res.sendFile(path.join(__dirname+'/views/signup.html'));
+	res.render('signup');
 });
 
 app.use('/', express.static(__dirname));
 
-var server = app.listen(3000, function(){
-	var host = server.address().address;
-	host = (host=== '::' ? 'localhost' : host);
-	var port = server.address().port;
-
-	console.log('listening at http://%s.%s', host, port);
-});
-
+app.listen(8080);
+console.log('running …');
