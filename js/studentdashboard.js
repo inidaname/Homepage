e593bd03e53f9e729a3880beae0db31d684564a0
Robert Ridley <robertridley@DCs-MacBook-Air.local>
2017-09-16 21:25:21 +0800

    updates to student dashboard

--- js/studentdashboard.js
+++ js/studentdashboard.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
+	$("#dash").addClass("sidebar-highlight");
+
 	var template = $("#dashboard-row").html();
 	var template2 = $("#dashboard-col").html();
 	$(".page-content").append(template);
-	for (var i = 0; i<10; i++){
+	for (var i = 0; i<4; i++){
 		$(".append-data").append(template2);
 	}
 });
