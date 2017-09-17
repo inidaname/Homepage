42d0d45621d9a12842760eca29264319f50e28b4
Daniel <danielr_soyyo@hotmail.com>
2017-09-17 11:39:38 +0100

    Create routes folder, fill .gitignore file and did few changes on server.js

--- server.js
+++ server.js
@@ -1,50 +1,37 @@
-var express = require('express');
-var app = express();
-// var path = require("path");
+const express = require('express'),
+    bodyParser = require('body-parser'),
+    morgan = require('morgan'),
+    path = require('path');
 
+//App configuration
+const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(morgan('dev'));
+
+//Middlewares
+app.use(bodyParser.urlencoded({ extended: true }))
+    .use(bodyParser.json());
+
+//Views
 app.set('view engine', 'ejs');
 
-app.get('/', function(req, res){
-	res.render('signup');
+//Routes
+app.get('/', function(req, res) {
+    res.render('signup');
 });
 
-app.get('/studentdashboard', function(req, res){
-	res.render('studentdashboard');
-});
+const studentProfile = require('./routes/studentProfile/studentProfile'),
+    studentPath = require('./routes/studentPath/studentPath'),
+    studentNetwork = require('./routes/studentNetwork/studentNetwork'),
+    helpCenter = require('./routes/helpCenter/help');
 
-app.get('/studentprofile', function(req, res){
-	res.render('studentprofile');
-});
+app.use(studentProfile)
+    .use(studentPath)
+    .use(studentNetwork)
+    .use(helpCenter)
 
-app.get('/studentsettings', function(req, res){
-	res.render('studentsettings');
-});
-
-app.get('/helpcenter', function(req, res){
-	res.render('helpcenter');
-});
-
-app.get('/studentstudy', function(req, res){
-	res.render('studentstudy');
-});
-
-app.get('/studentlearn', function(req, res){
-	res.render('studentlearn');
-});
-
-app.get('/studenttests', function(req, res){
-	res.render('studenttests');
-});
-
-app.get('/studentresults', function(req, res){
-	res.render('studentresults');
-});
-
-app.get('/studentnetwork', function(req, res){
-	res.render('studentnetwork');
-});
-
-app.use('/', express.static(__dirname));
-
-app.listen(8080);
-console.log('running …');
+//Server listener
+let port = process.env.PORT || 8000;
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}...`);
+})
